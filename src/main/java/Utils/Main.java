090d9ffab63dcb906ae051bc7cfb10a87bd6c612
shiva-prasad-reddy <puchalashivaprasadreddy@gmail.com>
2021-06-15 21:01:33 +0530

    --

--- src/main/java/Utils/Main.java
+++ src/main/java/Utils/Main.java
@@ -1,34 +1,12 @@
 package Utils;
 
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
 import java.util.HashSet;
 import java.util.TreeMap;
 
 import smile.math.matrix.Matrix;
 
 public class Main {
-
-	public static void serialize(Matrix matrix, String filename) throws IOException {
-		FileOutputStream fileOutputStream = new FileOutputStream(filename);
-		ObjectOutputStream objectOutputStream = new ObjectOutputStream(fileOutputStream);
-		objectOutputStream.writeObject(matrix);
-		objectOutputStream.close();
-		fileOutputStream.close();
-	}
-
-	public static Matrix deserialize(String filename) throws ClassNotFoundException, IOException {
-		FileInputStream fileInputStream = new FileInputStream(filename);
-		ObjectInputStream objectInputStream = new ObjectInputStream(fileInputStream);
-		Object object = objectInputStream.readObject();
-		objectInputStream.close();
-		fileInputStream.close();
-		return (Matrix) object;
-	}
 
 	public static Matrix scale(Matrix matrix, int a, int b) {
 		double min = Double.MAX_VALUE, max = Double.MIN_VALUE;
@@ -49,43 +27,29 @@ public class Main {
 
 	public static void main(String[] args) throws IOException, ClassNotFoundException {
 
-		String ant = "/home/shiva-prasad-reddy/Desktop/3/apache-ant-1.9.15";
+		String ant = "/home/shiva-prasad-reddy/Desktop/3/Tool1";
 		String javaKeywordsFile = "/home/shiva-prasad-reddy/Desktop/3/JavaKeywords.txt";
-
-		String referencesFN = "/home/shiva-prasad-reddy/Desktop/3/references";
-		String callsFN = "/home/shiva-prasad-reddy/Desktop/3/calls";
-		String semanticFN = "/home/shiva-prasad-reddy/Desktop/3/semantic";
 
 		Matrix references, calls, semantic;
 
-		try {
-			references = deserialize(referencesFN);
-			calls = deserialize(callsFN);
-			semantic = deserialize(semanticFN);
-		} catch (FileNotFoundException exception) {
-			ImportProject project = new ImportProject(ant);
-			Structural structural = new Structural(project);
-			references = structural.getReferences();
-			serialize(references, referencesFN);
-			calls = structural.getCalls();
-			serialize(calls, callsFN);
+		ImportProject project = new ImportProject(ant);
+		Structural _structural = new Structural(project);
+		references = _structural.getReferences();
+		calls = _structural.getCalls();
 
-			Semantic semanticDependencies = new Semantic(project.getIndex());
-			semanticDependencies.parseDocs(project.getAbstractSyntaxTrees(), javaKeywordsFile);
-			semanticDependencies.buildDocsMatrix();
-			semanticDependencies.computeInverseDocFreq();
-			semanticDependencies.computeTermFreq();
-			semanticDependencies.computeTF_IDF();
-			semantic = new Matrix(project.getAbstractSyntaxTrees().size(), project.getAbstractSyntaxTrees().size());
-			semanticDependencies.computeSimilarityMatrix(semantic);
-			serialize(semantic, semanticFN);
-		}
+		Semantic semanticDependencies = new Semantic(project.getIndex());
+		semanticDependencies.parseDocs(project.getAbstractSyntaxTrees(), javaKeywordsFile);
+		semanticDependencies.buildDocsMatrix();
+		semanticDependencies.computeInverseDocFreq();
+		semanticDependencies.computeTermFreq();
+		semanticDependencies.computeTF_IDF();
+		semantic = new Matrix(project.getAbstractSyntaxTrees().size(), project.getAbstractSyntaxTrees().size());
+		semanticDependencies.computeSimilarityMatrix(semantic);
 
 		references.replaceNaN(0.0);
 		calls.replaceNaN(0.0);
 		semantic.replaceNaN(0.0);
 
-		ImportProject project = new ImportProject(ant);
 		TreeMap<Integer, String> indexM = project.getIndexM();
 
 		int N = project.getAbstractSyntaxTrees().size();
@@ -111,20 +75,23 @@ public class Main {
 		for (int i = 0; i < N; i++)
 			cs[i] = 1 - 1 / (1 + t[i]);
 
-
 		// Utility Traces
-		double alpha = 0.95;
-		double beta = 0.95;
+		double alpha = 0.85;
+		double beta = 0.85;
 
 		HashSet<String> utilityTraces = new HashSet<>();
 		for (int i = 0; i < N; i++) {
 			if (fdr[i] > alpha && cs[i] > beta)
 				utilityTraces.add(indexM.get(i));
 		}
-		
+
 		System.out.println("Total Classes : " + N);
+		project.getAbstractSyntaxTrees().keySet().forEach(classname -> System.out.println("\t" + classname));
+
+		System.out.println();
+
 		System.out.println("Total Utility Traces : " + utilityTraces.size());
-//		utilityTraces.forEach(classname -> System.out.println(classname));
+		utilityTraces.forEach(classname -> System.out.println("\t" + classname));
 	}
 
 }
